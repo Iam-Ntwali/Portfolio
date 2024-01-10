d54a6ab7c7f4c884bd3fc6ec8b3dd108741e392b
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-01-10 13:07:45 +0200

    some changes 😄

--- src/scripts/themeToggle.js
+++ src/scripts/themeToggle.js
@@ -1,8 +1,21 @@
-const toggleTheme = document.querySelector('.theme-toggle');
+const toggleTheme = document.querySelector('#theme-toggle__btn');
 // console.log(toggleTheme);
+// state
+const theme = localStorage.getItem('theme');
 
-toggleTheme.addEventListener('click', () => {
+//on mount
+theme && document.body.classList.add(theme);
 
+const handleThemeToggle = () => {
   document.body.classList.toggle('light-mode');
 
-})
+  // keep theme state on document load
+  if (document.body.classList.contains('light-mode')) {
+    localStorage.setItem('theme', 'light-mode');
+  } else {
+    localStorage.removeItem('theme');
+    document.body.removeAttribute('class');
+  }
+}
+
+toggleTheme.addEventListener('click', handleThemeToggle)
