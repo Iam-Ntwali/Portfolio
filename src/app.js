ac1a97c52b6ad002fd3a5b98b11198c00e7abed5
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-08-02 13:19:56 +0200

    some in changes

--- src/app.js
+++ src/app.js
@@ -22,3 +22,19 @@ tabsButtons.forEach(tabBtn => {
     document.getElementById(tabId).classList.remove('hidden');
   });
 });
+
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('section--visible');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
